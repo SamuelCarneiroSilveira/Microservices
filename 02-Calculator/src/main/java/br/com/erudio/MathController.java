1b684b201b45c2314deb92d1e462521a6fd4a9ad
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-08-19 09:58:31 -0300

    Calculator

--- 02-Calculator/src/main/java/br/com/erudio/MathController.java
+++ 02-Calculator/src/main/java/br/com/erudio/MathController.java
@@ -22,7 +22,47 @@ public class MathController {
 		
 		return sum;
 	}
-
+	
+	@RequestMapping(value="/sub/{numberOne}/{numberTwo}",method=RequestMethod.GET)
+	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedMathOperationException("Please set a numeric value for subtraction");
+		}
+		
+		Double sub = convertToDouble(numberOne) - convertToDouble(numberTwo);
+		return sub;
+	}
+	
+	@RequestMapping(value="/mult/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedMathOperationException("Please set a numeric value for multiplication");
+		}
+		
+		Double mult = convertToDouble(numberOne) * convertToDouble(numberTwo);
+		return mult;
+	}
+	
+	@RequestMapping(value="/div/{numberOne}/{numberTwo}")
+	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsuportedMathOperationException("Please set a numeric value for division");
+		}
+		
+		Double div = convertToDouble(numberOne) / convertToDouble(numberTwo);
+		return div;
+	}
+	
+	@RequestMapping(value="/sqrt/{number}")
+	public Double sqrt(@PathVariable("number") String number) throws Exception {
+		if(!isNumeric(number)) {
+			throw new UnsuportedMathOperationException("Please set a numeric value for square");
+		}
+		
+		Double sqrt = Math.sqrt(convertToDouble(number));
+		return sqrt;
+	}
+	
 	private Double convertToDouble(String strNumber) {
 		if(strNumber == null) return 0D; // 0 Double 
 		String number = strNumber.replace(",", ".");
