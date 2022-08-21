d1ef5572acb31b3efef3afd5846999df49ec343b
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-08-21 14:48:39 -0300

    Lista de Pessoas

--- 03-CrudPessoas/src/main/java/br/com/erudio/services/PersonService.java
+++ 03-CrudPessoas/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -13,7 +15,6 @@ public class PersonService {
 	
 	public Person findById(String id) {
 		Person person = new Person();
-		
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Samuel");
 		person.setLastName("Carneiro");
@@ -22,4 +23,25 @@ public class PersonService {
 		return person;
 	}
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some Address in Brazil "+ i);
+		person.setGender("Gender "+ i);
+		return person;
+	}
+	
 }
+
