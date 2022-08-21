44484d9eff8a3e43327b3033966fa25068a74d28
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-08-21 09:48:07 -0300

    Implementando demais operações e separando em pacotes

--- 02-Calculator/src/main/java/br/com/erudio/controller/MathController.java
+++ 02-Calculator/src/main/java/br/com/erudio/controller/MathController.java
@@ -1,4 +1,4 @@
-package br.com.erudio;
+package br.com.erudio.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,74 +6,56 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.exception.UnsuportedMathOperationException;
+import br.com.erudio.math.SimpleMath;
+import br.com.erudio.request.converters.NumberConverter;
+
 
 @RestController
 public class MathController {
 	
-	
+	private SimpleMath math = new SimpleMath();
 	
 	@RequestMapping(value="/sum/{numberOne}/{numberTwo}",method=RequestMethod.GET) // quando não especifica, ele entende como GET
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Please set a numeric value");
-		}
-		
-		Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo); 
-		
-		return sum;
+		}		
+		return math.sum(NumberConverter.convertToDouble(numberOne),NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value="/sub/{numberOne}/{numberTwo}",method=RequestMethod.GET)
 	public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Please set a numeric value for subtraction");
 		}
-		
-		Double sub = convertToDouble(numberOne) - convertToDouble(numberTwo);
-		return sub;
+		return math.sub(NumberConverter.convertToDouble(numberOne),NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value="/mult/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Please set a numeric value for multiplication");
 		}
 		
-		Double mult = convertToDouble(numberOne) * convertToDouble(numberTwo);
-		return mult;
+		return math.mult(NumberConverter.convertToDouble(numberOne),NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value="/div/{numberOne}/{numberTwo}")
 	public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsuportedMathOperationException("Please set a numeric value for division");
 		}
-		
-		Double div = convertToDouble(numberOne) / convertToDouble(numberTwo);
-		return div;
+		return math.div(NumberConverter.convertToDouble(numberOne),NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value="/sqrt/{number}")
 	public Double sqrt(@PathVariable("number") String number) throws Exception {
-		if(!isNumeric(number)) {
+		if(!NumberConverter.isNumeric(number)) {
 			throw new UnsuportedMathOperationException("Please set a numeric value for square");
 		}
 		
-		Double sqrt = Math.sqrt(convertToDouble(number));
-		return sqrt;
-	}
-	
-	private Double convertToDouble(String strNumber) {
-		if(strNumber == null) return 0D; // 0 Double 
-		String number = strNumber.replace(",", ".");
-		if(isNumeric(number)) return Double.parseDouble(number);
-		return 0D; // 0 Double
+		return math.sqrt(NumberConverter.convertToDouble(number));
 	}
 
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) return false;
-		String number = strNumber.replace(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
 
 }
---
+++ 02-Calculator/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,20 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+
+		public Double sum(Double numberOne, Double numberTwo) {
+			return numberOne + numberTwo;
+		}
+		public Double sub(Double numberOne, Double numberTwo) {
+			return numberOne - numberTwo;
+		}
+		public Double mult(Double numberOne, Double numberTwo) {
+			return numberOne * numberTwo;
+		}
+		public Double div(Double numberOne, Double numberTwo) {
+			return numberOne / numberTwo;
+		}
+		public Double sqrt(Double number) {
+			return (Double) Math.sqrt(number);
+		}
+}
